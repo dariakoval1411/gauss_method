4f66735b14666b5f47a66aa5123660da9c36d140
dariakoval1411 <dashakoval1411@gmail.com>
2022-12-18 15:47:04 +0100

    elimination method change

--- src/gauss.c
+++ src/gauss.c
@@ -7,7 +7,21 @@
 int eliminate(Matrix *mat, Matrix *b){
     /**
   	 * Tutaj należy umieścić właściwą implemntację.
-		 */
+		 */	
+		int n=mat->r;
+
+		for(int c=0;c<n-1;r++)
+		{
+			for(int r=c+1;r<n;r++)
+			{
+				if(mat->data[c][c]==0)
+					return 1;
+					double m=mat->data[r][c]/mat->data[c][c];
+					for(int i=c;i<n;i++)
+						mat->data[r][i]-=m*(mat->data[c][i]);
+						b->data[r][0]-=m*(b->data[c][0]);
+			}
+		}
 
 		return 0;
 }
